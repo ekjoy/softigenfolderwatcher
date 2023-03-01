121da4daac753e4ba8806d802d1d5e198d772c8a
ekjoy <eldhosekjoy@outlook.com>
2023-03-01 16:30:53 +0000

    Basic Conversion as WindowsService

--- FileWatcher/Program.cs
+++ FileWatcher/Program.cs
@@ -1,11 +1,44 @@
 using FileWatcher;
+using Microsoft.Extensions.Logging.EventLog;
+using Microsoft.Extensions.Options;
 
+//////Settings for Worker service Running
+//IHost host = Host.CreateDefaultBuilder(args)
+//	.ConfigureServices(services =>
+//	{
+//		services.AddHostedService<Worker>();
+//		services.AddSingleton<IMyFileWatcher, MyFileWatcher>();
+//		services.AddScoped<IFileConsumerService, FileConsumerService>();
+
+//	})
+//	.Build();
+
+////Settings for WindowsService
 IHost host = Host.CreateDefaultBuilder(args)
+	.ConfigureLogging(options => {
+		if (OperatingSystem.IsWindows())
+		{
+			options.AddFilter<EventLogLoggerProvider>(level=>level >= LogLevel.Information);
+		}
+
+	})
 	.ConfigureServices(services =>
 	{
 		services.AddHostedService<Worker>();
 		services.AddSingleton<IMyFileWatcher, MyFileWatcher>();
 		services.AddScoped<IFileConsumerService, FileConsumerService>();
+		if (OperatingSystem.IsWindows())
+		{
+			services.Configure<EventLogSettings>(config =>
+			{
+				if (OperatingSystem.IsWindows())
+				{
+					config.LogName = "FolderWatcher Service";
+					config.SourceName = "Folderwatcher Service Source";
+				}
+			});
+
+		}
 
 	})
 	.Build();
